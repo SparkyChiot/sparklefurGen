e053c66f86b410b589e71ac99131bc323cc42acb
SparkyChiot <kylafaithsauber@gmail.com>
2026-03-19 17:03:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,104 +2,833 @@ const animals = [
   {
     name: "fox",
     body: "fur",
+    add: "",
   },
   {
     name: "coyote",
-    body: "fur"
+    body: "fur",
+    add: ""
   },
   {
     name: "wolf",
-    body: "fur"
+    body: "fur",
+    add: ""
   },
   {
     name: "house cat",
-    body: "fur"
+    body: "fur",
+    add: ""
   },
   {
     name: "scottish fold",
-    body: "fur"
+    body: "fur",
+    add: ""
   },
   {
     name: "sphinx",
-    body: "fur"
+    body: "fur",
+    add: ""
   },
   {
     name: "maine coon",
-    body: "fur"
+    body: "fur",
+    add: ""
   },
   {
     name: "lynx",
-    body: "fur"
+    body: "fur",
+    add: ""
   },
   {
     name: "caracal",
-    body: "fur"
+    body: "fur",
+    add: ""
   },
   {
     name: "tiger",
-    body: "fur"
+    body: "fur",
+    add: ""
   },
   {
     name: "lion",
-    body: "fur"
+    body: "fur",
+    add: ""
   },
   {
     name: "leopard",
-    body: "fur"
+    body: "fur",
+    add: ""
   },
   {
     name: "sabertooth",
-    body: "fur"
+    body: "fur",
+    add: ""
   },
   {
     name: "pig",
-    body: "hide"
+    body: "hide",
+    add: ""
   },
   {
     name: "sheep",
-    body: "wool"
+    body: "wool",
+    add: ""
   },
   {
     name: "goat",
-    body: "fur"
+    body: "fur",
+    add: ""
   },
   {
     name: "cow",
-    body: "hide"
+    body: "hide",
+    add: ""
   },
   {
     name: "horse",
-    body: "hide"
+    body: "hide",
+    add: ""
   },
   {
     name: "mule",
-    body: "hide"
+    body: "hide",
+    add: ""
   },
   {
     name: "donkey",
-    body: "hide"
+    body: "hide",
+    add: ""
   },
   {
     name: "unicorn",
-    body: "hide"
+    body: "hide",
+    add: ""
   },
   {
     name: "deer",
-    body: "hide"
+    body: "hide",
+    add: ""
   },
   {
     name: "caribou",
-    body: "hide"
+    body: "hide",
+    add: ""
   },
   {
     name: "pronghorn",
-    body: "fur"
+    body: "hide",
+    add: ""
   },
   {
-    name: "",
-    body: "fur"
+    name: "zebra",
+    body: "hide",
+    add: ""
+  },
+  {
+    name: "okapi",
+    body: "hide",
+    add: ""
+  },
+  {
+    name: "giraffe",
+    body: "hide",
+    add: ""
+  },
+  {
+    name: "tapir",
+    body: "hide",
+    add: ""
+  },
+  {
+    name: "mouse",
+    body: "fur",
+    add: ""
+  },
+  {
+    name: "rat",
+    body: "fur",
+    add: ""
+  },
+  {
+    name: "hamster",
+    body: "fur",
+    add: ""
+  },
+  {
+    name: "squirrel",
+    body: "fur",
+    add: ""
+  },
+  {
+    name: "beaver",
+    body: "fur",
+    add: ""
+  },
+  {
+    name: "capybara",
+    body: "fur",
+    add: ""
+  },
+  {
+    name: "weasel",
+    body: "fur",
+    add: ""
+  },
+  {
+    name: "skunk",
+    body: "fur",
+    add: ""
+  },
+  {
+    name: "river otter",
+    body: "fur",
+    add: ""
+  },
+  {
+    name: "sea otter",
+    body: "fur",
+    add: ""
+  },
+  {
+    name: "badger",
+    body: "fur",
+    add: ""
+  },
+  {
+    name: "wolverine",
+    body: "fur",
+    add: ""
+  },
+  {
+    name: "earthworm",
+    body: "skin",
+    add: ""
+  },
+  {
+    name: "grub",
+    body: "skin",
+    add: ""
+  },
+  {
+    name: "caterpillar",
+    body: "skin",
+    add: ""
+  },
+  {
+    name: "moth",
+    body: "fur",
+    add: ""
+  },
+  {
+    name: "snail",
+    body: "shell",
+    add: " a"
+  },
+  {
+    name: "snake",
+    body: "scales",
+    add: ""
+  },
+  {
+    name: "gecko",
+    body: "scales",
+    add: ""
+  },
+  {
+    name: "bearded dragon",
+    body: "scales",
+    add: ""
+  },
+  {
+    name: "frog",
+    body: "skin",
+    add: ""
+  },
+  {
+    name: "axolotl",
+    body: "skin",
+    add: ""
+  },
+  {
+    name: "bunny",
+    body: "fur",
+    add: ""
+  },
+  {
+    name: "raccoon",
+    body: "fur",
+    add: ""
+  },
+  {
+    name: "tanuki",
+    body: "fur",
+    add: ""
+  },
+  {
+    name: "hyena",
+    body: "fur",
+    add: ""
+  },
+  {
+    name: "aardwolf",
+    body: "fur",
+    add: ""
+  },
+  {
+    name: "anteater",
+    body: "fur",
+    add: ""
+  },
+  {
+    name: "bear",
+    body: "fur",
+    add: ""
+  },
+  {
+    name: "elephant",
+    body: "hide",
+    add: ""
+  },
+  {
+    name: "mammoth",
+    body: "fur",
+    add: ""
+  },
+  {
+    name: "seal",
+    body: "fur",
+    add: ""
+  },
+  {
+    name: "walrus",
+    body: "fur",
+    add: ""
+  },
+  {
+    name: "shark",
+    body: "skin",
+    add: ""
+  },
+  {
+    name: "jackalope",
+    body: "fur",
+    add: ""
+  },
+  {
+    name: "western dragon",
+    body: "scales",
+    add: ""
+  },
+  {
+    name: "eastern dragon",
+    body: "fur",
+    add: ""
+  },
+  {
+    name: "wyvern",
+    body: "scales",
+    add: ""
+  },
+  {
+    name: "ant",
+    body: "exoskeleton",
+    add: " a"
+  },
+  {
+    name: "slug",
+    body: "skin",
+    add: ""
+  },
+  {
+    name: "millipede",
+    body: "exoskeleton",
+    add: " a"
+  },
+  {
+    name: "isopod",
+    body: "exoskeleton",
+    add: " a"
+  },
+  {
+    name: "scorpion",
+    body: "exoskeleton",
+    add: " a"
+  },
+  {
+    name: "shrimp",
+    body: "exoskeleton",
+    add: " a"
+  },
+  {
+    name: "fly",
+    body: "exoskeleton",
+    add: " a"
+  },
+  {
+    name: "bee",
+    body: "fur",
+    add: ""
+  },
+  {
+    name: "firefly",
+    body: "exoskeleton",
+    add: " a"
+  },
+  {
+    name: "dragonfly",
+    body: "exoskeleton",
+    add: " a"
+  },
+  {
+    name: "butterfly",
+    body: "wings",
+    add: ""
+  },
+  {
+    name: "beetle",
+    body: "exoskeleton",
+    add: " a"
+  },
+  {
+    name: "weevil",
+    body: "exoskeleton",
+    add: " a"
+  },
+  {
+    name: "spider",
+    body: "fur",
+    add: ""
+  },
+  {
+    name: "mantis",
+    body: "exoskeleton",
+    add: " a"
+  },
+  {
+    name: "jersey devil",
+    body: "fur",
+    add: ""
+  },
+  {
+    name: "kelpie",
+    body: "scales",
+    add: ""
+  },
+  {
+    name: "chupacabra",
+    body: "skin",
+    add: ""
+  },
+  {
+    name: "squonk",
+    body: "hide",
+    add: ""
+  },
+  {
+    name: "fur bearing trout",
+    body: "fur",
+    add: ""
+  },
+  {
+    name: "direwolf",
+    body: "fur",
+    add: ""
+  },
+  {
+    name: "honshu wolf",
+    body: "fur",
+    add: ""
+  },
+  {
+    name: "cave hyena",
+    body: "fur",
+    add: ""
+  },
+  {
+    name: "smilodon",
+    body: "fur",
+    add: ""
+  },
+  {
+    name: "wallaby",
+    body: "fur",
+    add: ""
+  },
+  {
+    name: "pigeon",
+    body: "feathers",
+    add: ""
+  },
+  {
+    name: "dodo",
+    body: "feathers",
+    add: ""
+  },
+  {
+    name: "dolphin",
+    body: "skin",
+    add: ""
+  },
+  {
+    name: "chicken",
+    body: "feathers",
+    add: ""
+  },
+  {
+    name: "goose",
+    body: "feathers",
+    add: ""
+  },
+  {
+    name: "duck",
+    body: "feathers",
+    add: ""
+  },
+  {
+    name: "rabbit",
+    body: "fur",
+    add: ""
+  },
+  {
+    name: "parrot",
+    body: "feathers",
+    add: ""
+  },
+  {
+    name: "moose",
+    body: "hide",
+    add: ""
   },
 ];
+
+const dogBreeds = [
+  "golden retriever",
+  "setter",
+  "border collie",
+  "schnauzer",
+  "dachshund",
+  "pug",
+  "husky",
+  "german shepherd",
+  "doberman",
+  "great dane",
+  "chihuahua",
+  "corgi",
+  "airedale terrier",
+  "border collie",
+  "australian shepherd",
+  "clumber spaniel",
+  "american pit bull terrier",
+  "borzoi",
+  "australian cattle dog",
+  "german shepherd",
+  "akita inu",
+  "bull terrier",
+  "greyhound",
+  "afghan hound",
+  "american hairless terrier",
+  "kangal shepherd"
+  "bernese mountain dog",
+  "boxer",
+  "bulldog",
+  "great pyrenees",
+];
+
+const pronouns = [
+  {
+    name: "she",
+    name2: "her",
+    verb: "has",
+    posessive: "her"
+  },
+  {
+    name: "he",
+    name2: "him",
+    verb: "has",
+    posessive: "his"
+  },
+  {
+    name: "they",
+    name2: "them",
+    verb: "have",
+    posessive: "their"
+  },
+  {
+    name: "it",
+    name2: "its",
+    verb: "has",
+    posessive: "its"
+  },
+  {
+    name: "xey",
+    name2: "xem",
+    verb: "have",
+    posessive: "xeir"
+  },
+  {
+    name: "fey",
+    name2: "faer",
+    verb: "have",
+    posessive: "faer"
+  },
+  {
+    name: "ey",
+    name2: "em",
+    verb: "have",
+    posessive: "eir"
+  },
+];
+
+const hasTraits = [
+  "one eye",
+  "spikes",
+  "demon horns",
+  "dragon horns",
+  "goat horns",
+  "a unicorn horn",
+  "antlers"
+  "demon wings",
+  "dragon wings",
+  "angel wings",
+  "bat wings",
+  "bird wings",
+  "fairy wings",
+  "beetle wings",
+  "robotic wings",
+  "gear wings",
+  "a halo",
+  "extra eyes",
+  "robotic limbs",
+  "exposed bones",
+  "stitches",
+  "tentacle limbs",
+  "tusks",
+  "saber teeth",
+  "scars",
+  "belly mouth"
+  "fish fins",
+  "shark fins",
+  "antennae",
+  "a rat tail",
+  "a dino tail",
+  "a shark tail",
+  "a massive tail",
+  "a plush tail",
+  "a mouth tail",
+  "a gummi worm tail",
+  "oni fangs",
+  "monster claws",
+  "glowing markings",
+  "multiple tails",
+  "heterochromia",
+  "star pupils",
+  "cat pupils",
+  "x pupils",
+  "goat pupils",
+  "square pupils",
+  "diamond pupils",
+  "heart pupils",
+  "m shaped pupils",
+  "no pupils",
+  "extra arms",
+  "extra legs",
+  "extra ears",
+  "a synth voice",
+  "helix piercings",
+  "industrial piercings",
+  "orbital piercings",
+  "a bridge piercing"
+  "an eyebrow piercing",
+  "nostril piercings",
+  "a septum piercing",
+  "a monroe piercing",
+  "a vertical labret piercing",
+  "snake bites",
+  "angel bites",
+  "spider bites",
+  "dolphin bites",
+  "dahlia bites",
+  "canine bites",
+  "shark bites",
+  "a tongue piercing",
+  "gauged ears",
+  "dimple piercings",
+  "venom piercings",
+];  
+
+const hasMarkings = [
+  "x pattern",
+  "checkers",
+  "tiger stripe",
+  "leopard print",
+  "cheetah print",
+  "splatter",
+  "gradient",
+  "skeleton",
+  "skull pattern",
+  "star pattern",
+  "heart pattern",
+  "galaxy print",
+  "cloud pattern",
+  "flower print",
+  "spiral pattern",
+  "ring",
+  "standard",
+  "spots",
+  "polka dot",
+  "tuxedo print",
+  "grafitti",
+  "glitch",
+  "cel-shaded",
+];
+
+const isPersonalities = [
+  "overly dramatic",
+  "feral but polite",
+  "chronically confused",
+  "incapable of being quiet",
+  "easily distracted",
+  "always hungry",
+  "easily distracted",
+  "allergic to normal people",
+  "addicted to caffeine",
+  "a menace to society",
+  "photogenic",
+  "stuck in the 2000s",
+  "stoic but has a great sense of humor",
+  "HYPER ALL THE TIME"
+];
+
+const isBackgrounds = [
+  "a failed lab experiment",
+  "a runaway freakshow act",
+  "a fallen star",
+  "the chosen one",
+  "the last of their kind",
+  "a cryptid",
+  "from a timeline that no longer exists",
+  "a reincarnation",
+  "from a dream",
+  "an abandoned prototype",
+  "a clone who wasn't supposed to have free will",
+  "an urban legend that turned out to be true",
+  "on the run",
+  "an exile",
+  "secretly royalty",
+  "a cursed doll",
+  "haunted by a ghost",
+  "a shapeshifter"
+];
+
+const aesthetics = [
+  "emo",
+  "scene",
+  "y2k",
+  "e-boy/girl",
+  "gyaru",
+  "art deco",
+  "art nouveau",
+  "art hoe",
+  "indie",
+  "grunge",
+  "hyperpop",
+  "preppy",
+  "pastel",
+  "lolita",
+  "coquette",
+  "vintage",
+  "flapper",
+  "wanderlust",
+  "fruitiger aero",
+  "americana",
+  "vulture culture",
+  "analog horror",
+  "liminal space",
+  "bubblegum bitch",
+  "60's",
+  "70's",
+  "80's heartthrob",
+  "2020 alt",
+  "2014 tumblr",
+  "90's anime",
+  "1950s suburbia",
+  "2000s autumn",
+  "angura kei",
+  "decora kei",
+  "fairy kei",
+  "party kei",
+  "tanbi kei",
+  "anglo gothic",
+  "midwest gothic",
+  "southern gothic",
+  "acidwave",
+  "vaporwave",
+  "mall goth",
+  "pastel goth",
+  "cyber goth",
+  "woodland goth",
+  "whimsigoth",
+  "corporate goth",
+  "trad goth",
+  "glamrock",
+  "indie rock",
+  "bardcore",
+  "balletcore",
+  "arcadecore",
+  "horrorcore",
+  "cryptidcore",
+  "lovecore",
+  "gremlincore",
+  "cottagecore",
+  "angelcore",
+  "adventurecore",
+  "barbiecore",
+  "clowncore",
+  "kidcore",
+  "candycore",
+  "dollcore",
+  "ghostcore",
+  "nostalgiacore",
+  "crowcore",
+  "rainbowcore",
+  "spacecore",
+  "trashcore",
+  "witchcore",
+  "bastardcore",
+  "weirdcore",
+  "dreamcore",
+  "webcore",
+  "atompunk",
+  "solarpunk",
+  "cyberpunk",
+  "voidpunk",
+  "lunarpunk",
+  "junkpunk",
+  "steampunk",
+  "urbanpunk",
+  "biopunk",
+  "aetherpunk",
+  "corporate punk",
+  "crust punk",
+  "cripplepunk",
+  "deiselpunk",
+  "punk",
+  "dark academia",
+  "light academia",
+  "cryptid academia",
+  "horror academia",
+  "witch academia",
+  "art academia",
+  "chaotic academia",
+  "fairy academia",
+  "romantic academia",
+  "goblin academia",
+  "pastel academia",
+  "vintage academia",
+  "darkest academia",
+  "green academia",
+];
+
 const palettes = [
   {
     name: "Olive Field",
